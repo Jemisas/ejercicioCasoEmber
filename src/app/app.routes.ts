95588b46dde8eb7b6b7b25844e05324b553916cf
Jeremy Joel Quispe Andia <u202216279@upc.edu.pe>
2024-05-05 14:22:16 -0500

    chore: changed routes of components on routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {BundlePageComponent} from "./store/pages/bundle-page/bundle-page.component";
-import {HomeComponent} from "../public/pages/home/home.component";
-import {PageNotFoundComponent} from "../public/pages/page-not-found/page-not-found.component";
+import {HomeComponent} from "./public/pages/home/home.component";
+import {PageNotFoundComponent} from "./public/pages/page-not-found/page-not-found.component";
 
 export const routes: Routes = [
   { path: 'home', component: HomeComponent },
